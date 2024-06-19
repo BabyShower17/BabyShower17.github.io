c3748c7266820832055999f34b6b95177a0f75d7
BabyShower17 <ivanovich.aipr@gmail.com>
2024-06-19 00:11:52 -0600

    script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// script.js
+
 document.addEventListener('DOMContentLoaded', function () {
     const startButton = document.getElementById('start-button');
     const audio = document.getElementById('bg-music');
@@ -63,14 +65,6 @@ document.addEventListener('DOMContentLoaded', function () {
                     countdownTimer.innerHTML = "<div class='countdown-container'><p>¡El evento ha comenzado!</p></div>";
                 }
             }, 1000);
-        });
-    });
-
-    // Agregar eventos touchstart para compatibilidad con dispositivos táctiles
-    const areas = document.querySelectorAll('area');
-    areas.forEach(area => {
-        area.addEventListener('touchstart', function () {
-            window.location.href = area.href;
         });
     });
 });
